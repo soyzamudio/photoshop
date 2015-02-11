c018dd5314dc093c3c709f1ecc2f9e0ab49bce52
Jose Zamudio <zamudio@outlook.com>
2015-02-11 14:56:35 -0800

    Add dynamic background and canvas

--- client/main.js
+++ client/main.js
@@ -3,8 +3,57 @@
 $(document).ready(function() {
   $('#add-color').click(addColor);
   $('#add-random-colors').click(addRandom);
-
+  $('#start').click(clickStart);
+  $('#stop').click(clickStop);
+  $('#add-canvas').click(clickAddCanvas);
+  $('#colors').on('click', '.box', clickBox);
+  $('#canvas').on('mouseenter', '.tiny', enterTiny);
 });
+
+var colors = [];
+var timer;
+var index = 0;
+
+function enterTiny() {
+  var color = $('#selected').css('background-color');
+  $(this).css('background-color', color);
+}
+
+function clickAddCanvas() {
+  var size = $('#canvas-size').val() * 1;
+  for (var i = 0; i < size; i++) {
+    var $tiny = $('<div>');
+    $tiny.addClass('tiny');
+    $('#canvas').append($tiny);
+  }
+}
+
+function clickBox() {
+  $('#selected').css('background-color', $(this).css('background-color'));
+}
+
+function clickStart() {
+  colors = $('.box').toArray().map(function(e) {
+    return $(e).css('background-color');
+  });
+  index = 0;
+  clearInterval(timer);
+  timer = setInterval(looping, 250);
+}
+
+function clickStop() {
+  clearInterval(timer);
+}
+
+function looping() {
+  var color = colors[index];
+  $('body').css('background-color', color);
+  if (index < colors.length) {
+    index++;
+  } else {
+    index = 0;
+  }
+}
 
 function addColor() {
   var color = $('#color').val();
@@ -25,7 +74,6 @@ function addRandom() {
              ', ' + randomColor(256) +
              ')');
     $box.addClass('box');
-
     $('#colors').append($box);
   }
 }
